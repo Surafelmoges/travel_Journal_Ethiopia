f2b757be9fd5b59dee52134c2ed5672c2fc02726
surafel <surafelmoges01@gmail.com>
2025-06-05 19:39:06 +0100

    Insert a data or file in data.jsx file

---
+++ src/componets/Data.jsx
@@ -0,0 +1,95 @@
+const data = [
+  {
+    id: 1,
+    img: {
+      src: "https://cdn.pixabay.com/photo/2019/04/10/17/12/ethiopia-4117559_1280.jpg",
+      alt: "Lalibela",
+    },
+    icon: "https://media.istockphoto.com/id/1442229802/photo/destination-3d-icon-map-pointer-icon-gps-location-symbol-3d-red-glass-pointer-3d-rendering.jpg?s=1024x1024&w=is&k=20&c=j77t1e700CBHk7mzloSfyuVTgqNEIxbx5pi9NqGgljk=",
+    country: "Ethiopia",
+    mapLink: "https://www.google.com/maps/place/Lalibela",
+    date: "05-06-2025",
+    detail:
+      "Lalībela, religious and pilgrimage centre in north-central Ethiopia, is famous for its 11 medieval rock-hewn churches built during the reign of King Lalibela."
+  },
+  {
+    id: 2,
+    img: {
+      src: "https://cdn.pixabay.com/photo/2016/11/14/04/30/ethiopia-1822593_1280.jpg",
+      alt: "Simien Mountains",
+    },
+    icon: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+    country: "Ethiopia",
+    mapLink: "https://www.google.com/maps/place/Simien+Mountains+National+Park",
+    date: "06-06-2025",
+    detail:
+      "The Simien Mountains are a rugged range in northern Ethiopia, known for dramatic scenery and rare wildlife like the Gelada baboon and Walia ibex."
+  },
+  {
+    id: 3,
+    img: {
+      src: "https://cdn.pixabay.com/photo/2021/01/27/11/36/axum-5953657_1280.jpg",
+      alt: "Axum Obelisks",
+    },
+    icon: "https://cdn-icons-png.flaticon.com/512/854/854878.png",
+    country: "Ethiopia",
+    mapLink: "https://www.google.com/maps/place/Aksum",
+    date: "07-06-2025",
+    detail:
+      "Axum, the ancient capital of the Aksumite Empire, is known for its tall granite obelisks and is believed to be home to the Ark of the Covenant."
+  },
+  {
+    id: 4,
+    img: {
+      src: "https://cdn.pixabay.com/photo/2021/12/19/17/51/ethiopia-6881144_1280.jpg",
+      alt: "Blue Nile Falls",
+    },
+    icon: "https://cdn-icons-png.flaticon.com/512/684/684853.png",
+    country: "Ethiopia",
+    mapLink: "https://www.google.com/maps/place/Blue+Nile+Falls",
+    date: "08-06-2025",
+    detail:
+      "The Blue Nile Falls, known locally as 'Tis Issat' (smoking water), are one of Ethiopia's most impressive natural sights."
+  },
+  {
+    id: 5,
+    img: {
+      src: "https://cdn.pixabay.com/photo/2023/03/01/18/34/ethiopia-7823734_1280.jpg",
+      alt: "Gondar Castles",
+    },
+    icon: "https://cdn-icons-png.flaticon.com/512/3595/3595455.png",
+    country: "Ethiopia",
+    mapLink: "https://www.google.com/maps/place/Gondar",
+    date: "09-06-2025",
+    detail:
+      "Gondar, once the imperial capital, is home to the Fasil Ghebbi royal enclosure with its iconic castles and palaces."
+  },
+  {
+    id: 6,
+    img: {
+      src: "https://cdn.pixabay.com/photo/2020/02/28/07/16/ethiopia-4885972_1280.jpg",
+      alt: "Danakil Depression",
+    },
+    icon: "https://cdn-icons-png.flaticon.com/512/1483/1483336.png",
+    country: "Ethiopia",
+    mapLink: "https://www.google.com/maps/place/Danakil+Depression",
+    date: "10-06-2025",
+    detail:
+      "The Danakil Depression is one of the hottest and most geologically active places on Earth, featuring colorful acid pools and salt flats."
+  },
+  {
+    id: 7,
+    img: {
+      src: "https://cdn.pixabay.com/photo/2022/11/07/11/43/addis-ababa-7575917_1280.jpg",
+      alt: "Addis Ababa",
+    },
+    icon: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+    country: "Ethiopia",
+    mapLink: "https://www.google.com/maps/place/Addis+Ababa",
+    date: "11-06-2025",
+    detail:
+      "Addis Ababa, the capital city of Ethiopia, is home to the African Union, museums, and vibrant culture, blending modern life with rich history."
+  }
+];
+
+export default data;
